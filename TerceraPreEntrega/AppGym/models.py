fc8265f4b89d83ec924bb503b280d7db1cdcb4e8
Antonella Loza <anto.loza.8@hotmail.com>
2023-08-28 15:59:16 -0300

    Punto Seguro

--- TerceraPreEntrega/AppGym/models.py
+++ TerceraPreEntrega/AppGym/models.py
@@ -7,6 +7,9 @@ class Rutina(models.Model):
     rutina = models.CharField(max_length=50)
     duracion = models.IntegerField()
 
+    def __str__(self):
+        return f"ID_rutina: {self.ID_rutina} - Rutina: {self.rutina} - Duracion: {self.duracion}"
+
 class Clientes(models.Model):
     usuario = models.CharField(max_length=50)
     nombre = models.CharField(max_length=50)
@@ -22,7 +25,6 @@ class Clientes(models.Model):
 class Profesora(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
-
 
 
 
